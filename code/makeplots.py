8d1cc267c5928a572723552a308e61fae7aeeb8d
davidwhogg <david.hogg@nyu.edu>
2015-07-28 16:38:10 +0200

    face on, kids

--- code/makeplots.py
+++ code/makeplots.py
@@ -10,6 +10,34 @@ def hogg_savefig(fn, figure=None):
         figure.savefig(fn)
     print "hogg_savefig(): Wrote %s" % fn
 
+
+def make_grid(xs, xmin, xmax, dx, ys, ymin, ymax, dy, data):
+    young = 4. # Gyr
+    # end magic number time
+    xgrid = np.arange(xmin + 0.5 * dx, xmax, dx)
+    nx = len(xgrid)
+    ygrid = np.arange(ymin + 0.5 * dy, ymax, dy)
+    ny = len(ygrid)
+    xgrid = xgrid[:, None] * (np.ones(ny))[None, :]
+    ygrid = (np.ones(nx))[:, None] * ygrid[None, :]
+    Nstars = np.zeros_like(xgrid)
+    young_fracs = np.zeros_like(xgrid) + np.NaN
+    low_alpha_fracs = np.zeros_like(xgrid) + np.NaN
+    for xxi in range(nx):
+        for yyi in range(ny):
+            x, y = xgrid[xxi, yyi], ygrid[xxi, yyi]
+            inside = ((xs > x - dx) *
+                      (xs < x + dx) *
+                      (ys > y - dy) *
+                      (ys < y + dy))
+            N = np.float(np.sum(inside))
+            Nstars[xxi, yyi] = N
+            if N > 8: # magic 8
+                young_fracs[xxi, yyi] = np.sum(data[inside, 9] < young) / N
+                low_alpha_fracs[xxi, yyi] = np.sum(data[inside, 8] <
+                    0.05 - 0.2 * data[inside, 7]) / N # magic number insanity
+    return xgrid, ygrid, Nstars, young_fracs, low_alpha_fracs
+
 if __name__ == "__main__":
     print "Hello World!"
 
@@ -33,30 +61,10 @@ if __name__ == "__main__":
     Rmin, Rmax = 5., 14. # kpc
     zmin, zmax = -1.5, 1.5 # kpc
     dR = 0.1 # kpc
-    young = 4. # Gyr
-    alphacut = 0.05 # dex
-    # end magic number time
-    Rgrid = np.arange(Rmin + 0.5 * dR, Rmax, dR)
-    nR = len(Rgrid)
-    zgrid = np.arange(zmin + 0.5 * dR, zmax, dR)
-    nz = len(zgrid)
-    Rgrid = Rgrid[:, None] * (np.ones(nz))[None, :]
-    zgrid = (np.ones(nR))[:, None] * zgrid[None, :]
-    Nstars = np.zeros_like(Rgrid)
-    young_fracs = np.zeros_like(Rgrid) + np.NaN
-    low_alpha_fracs = np.zeros_like(Rgrid) + np.NaN
-    for rri in range(nR):
-        for zzi in range(nz):
-            R, z = Rgrid[rri, zzi], zgrid[rri, zzi]
-            inside = ((data[:, 2] > R - dR) *
-                      (data[:, 2] < R + dR) *
-                      (data[:, 4] > z - dR) *
-                      (data[:, 4] < z + dR))
-            N = np.float(np.sum(inside))
-            Nstars[rri, zzi] = N
-            if N > 8: # magic 8
-                young_fracs[rri, zzi] = np.sum(data[inside, 9] < young) / N
-                low_alpha_fracs[rri, zzi] = np.sum(data[inside, 8] < alphacut) / N
+    (Rgrid, zgrid, Nstars, young_fracs,
+     low_alpha_fracs) = make_grid(data[:, 2], Rmin, Rmax, dR,
+                                  data[:, 4], zmin, zmax, dR, data)
+
     plt.figure(figsize=(12,4))
     plt.clf()
     imshow_kwargs = {"interpolation": "nearest",
@@ -68,7 +76,7 @@ if __name__ == "__main__":
     plt.xlabel("Galactocentric radius $R$ (kpc)")
     plt.ylabel("Galactic height $z$ (kpc)")
     foo = plt.colorbar()
-    foo.set_label("fraction of stars with age < %0.1f Gyr" % young)
+    foo.set_label("fraction of stars with low age")
     hogg_savefig("young_fracs.png")
 
     plt.clf()
@@ -77,7 +85,7 @@ if __name__ == "__main__":
     plt.xlabel("Galactocentric radius $R$ (kpc)")
     plt.ylabel("Galactic height $z$ (kpc)")
     foo = plt.colorbar()
-    foo.set_label("fraction of stars with %s < %0.2f" % (names[8], alphacut))
+    foo.set_label("fraction of stars with low chemical age")
     hogg_savefig("low_alpha_fracs.png")
 
     plt.clf()
@@ -90,8 +98,53 @@ if __name__ == "__main__":
                   (2. * dR, 2 * dR))
     hogg_savefig("nstars.png")
 
-    print "Making triangle plot ..."
-    figure = triangle.corner(data[:, 1:], labels=names[1:])
-    hogg_savefig("triangle.png", figure=figure)
+    print "Making (x,y) grid..."
+    # start magic number time
+    inplane = (data[:, 4] < 0.30) * (data[:, 4] > -0.30) # kpc
+    xs = data[inplane, 2] * np.cos(data[inplane, 3])
+    ys = data[inplane, 2] * np.sin(data[inplane, 3])
+    xmin, xmax = 5., 14.
+    ymin, ymax = -4.5, 4.5
+    dR = 0.1 # kpc
+    (xgrid, ygrid, Nstars, young_fracs,
+     low_alpha_fracs) = make_grid(xs, xmin, xmax, dR,
+                                  ys, ymin, ymax, dR, data[inplane])
+
+    plt.figure(figsize=(5,4))
+    plt.clf()
+    imshow_kwargs = {"interpolation": "nearest",
+                     "aspect": "equal",
+                     "origin": "lower",
+                     "extent": (xmin, xmax, ymin, ymax)}
+    plt.imshow(young_fracs.T, cmap=plt.cm.RdBu, vmin=0., vmax=1.,
+               **imshow_kwargs)
+    plt.xlabel(r"Galactocentric $R\,\cos\phi$ (kpc)")
+    plt.ylabel(r"Galactocentric $R\,\sin\phi$ (kpc)")
+    foo = plt.colorbar()
+    foo.set_label("fraction of stars with low age")
+    hogg_savefig("fo_young_fracs.png")
+
+    plt.clf()
+    plt.imshow(low_alpha_fracs.T, cmap=plt.cm.RdBu, vmin=0., vmax=1.,
+               **imshow_kwargs)
+    plt.xlabel(r"Galactocentric $R\,\cos\phi$ (kpc)")
+    plt.ylabel(r"Galactocentric $R\,\sin\phi$ (kpc)")
+    foo = plt.colorbar()
+    foo.set_label("fraction of stars with low chemical age")
+    hogg_savefig("fo_low_alpha_fracs.png")
+
+    plt.clf()
+    plt.imshow(Nstars.T, cmap=plt.cm.afmhot, vmin=0.,
+               **imshow_kwargs)
+    plt.xlabel(r"Galactocentric $R\,\cos\phi$ (kpc)")
+    plt.ylabel(r"Galactocentric $R\,\sin\phi$ (kpc)")
+    foo = plt.colorbar()
+    foo.set_label(r"number of stars in a $%.1f\times%.1f$ kpc box" %
+                  (2. * dR, 2 * dR))
+    hogg_savefig("fo_nstars.png")
+
+#    print "Making triangle plot ..."
+#    figure = triangle.corner(data[:, 1:], labels=names[1:])
+#    hogg_savefig("triangle.png", figure=figure)
 
     print "Goodbye World!"
