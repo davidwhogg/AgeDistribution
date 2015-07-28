93dbef7190f73ff3dc0feaa5163fc887a99fe11b
davidwhogg <david.hogg@nyu.edu>
2015-07-28 11:57:01 +0200

    one figure

--- code/makeplots.py
+++ code/makeplots.py
@@ -1,5 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def hogg_savefig(fn):
+    print "hogg_savefig(): Writing %s ..." % fn
+    plt.savefig(fn)
+    print "hogg_savefig(): Wrote %s" % fn
+
 if __name__ == "__main__":
     print "Hello World!"
+
+    fn = "../data/HWR_redclump_sample.txt"
+    print "Reading %s ..." % fn
+    data = np.genfromtxt(fn)
+    print data[2]
+    print data.shape
+    print "Read %s" % fn
+
+    print "Making age histogram ..."
+    plt.clf()
+    plt.hist(data[:, -1], bins=32, histtype="step")
+    plt.xlabel("age (Gyr)")
+    hogg_savefig("histogram.png")
+
+    print "Goodbye World!"
